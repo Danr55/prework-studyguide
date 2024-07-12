9855a72d7b728e7e182dc0bc18fef9931a066c5b
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-07-12 16:58:15 -0700

    Added JS, created array

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+/*
+const topic ="HTML"
+
+if(topic === 'HTML') {
+    console.log("Let's study HTML!");
+}else if (topic === 'CSS'){
+    console.log("Let's study CSS!");
+}else if(topic === 'Git') {
+    console.log("Let's study Git!");
+}else if(topic === 'Javascript'){
+    console.log("Let's study Javascript!");
+}else{
+    console.log('Please try again!')
+}
+
+const shapes = ["triangle","square","pentagon","circle"];
+console.log(shapes[0])
+*/
+
+const topics = ['HTML','CSS','Git','Javascript']
+for(let x = 0; x <topics.length; x++){
+    console.log(topics[x]);
+}
