8bc76c15c27b84713ba2d952e02965bba92d1738
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-07-12 17:47:33 -0700

    Created JS functions added notes JS

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,28 @@
-const topics = ['HTML','CSS','Git','Javascript']
-for(let x = 0; x <topics.length; x++){
-    console.log(topics[x]);
-}
 
-/*
-const topic ="HTML"
+const topics = ['HTML','CSS','Git','Javascript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-if(topic === 'HTML') {
+function listTopics(){
+    for(let x = 0; x <topics.length; x++){
+        console.log(topics[x]);
+    }
+    }
+
+function selectTopic() {
+if(randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-}else if (topic === 'CSS'){
+}else if (randomTopic === 'CSS'){
     console.log("Let's study CSS!");
-}else if(topic === 'Git') {
+}else if(randomTopic === 'Git') {
     console.log("Let's study Git!");
-}else if(topic === 'Javascript'){
+}else if(randomTopic === 'Javascript'){
     console.log("Let's study Javascript!");
 }else{
-    console.log('Please try again!')
+    console.log('Please try again!');
+    }
 }
 
-const shapes = ["triangle","square","pentagon","circle"];
-console.log(shapes[0])
-*/
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
