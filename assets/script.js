72e650676100192ff41ca49f9915b17a596dd5a3
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-07-12 17:01:17 -0700

    updated JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+const topics = ['HTML','CSS','Git','Javascript']
+for(let x = 0; x <topics.length; x++){
+    console.log(topics[x]);
+}
+
 /*
 const topic ="HTML"
 
@@ -16,8 +21,3 @@ if(topic === 'HTML') {
 const shapes = ["triangle","square","pentagon","circle"];
 console.log(shapes[0])
 */
-
-const topics = ['HTML','CSS','Git','Javascript']
-for(let x = 0; x <topics.length; x++){
-    console.log(topics[x]);
-}
